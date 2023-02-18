ea6b0748b830a31f42014f47bc3b7e99f4b99568
vincent178 <vh7157@gmail.com>
2023-02-18 21:02:02 +0800

    fix

--- lua/nvim-github-linker.lua
+++ lua/nvim-github-linker.lua
@@ -1,10 +1,13 @@
-local function github_linker(start_line, end_line)
+local M = {}
+
+function M.github_linker(start_line, end_line)
     local line_number = start_line or vim.fn.line('.')
     local end_line_number = end_line or line_number
     local file_name = vim.fn.expand('%:t')
     local repo_url = vim.fn.systemlist("git config --get remote.origin.url")[1]
     local repo_path = vim.fn.substitute(repo_url, '\\(.*github.com\\)\\(:\\|/\\)\\([^/]*\\)/\\(.*\\)\\.git',
         'https://github.com/\\3/\\4', '')
+    print(repo_path)
     local branch_name = vim.fn.systemlist("git symbolic-ref --short HEAD")[1]
     local base_url = repo_path .. '/blob/' .. branch_name
     local range = 'L' .. line_number
@@ -16,16 +19,18 @@ local function github_linker(start_line, end_line)
     return full_url
 end
 
-local function github_linker_command(...)
+function M.github_linker_command(...)
     local args = { ... }
     local repo = vim.fn.systemlist("git config --get remote.origin.url")[1]
     local branch = vim.fn.systemlist("git symbolic-ref --short HEAD")[1]
     local start_line, end_line = args[1], args[2]
     if start_line and end_line then
-        print(github_linker(start_line, end_line))
+        print(M.github_linker(start_line, end_line))
     else
-        print(github_linker())
+        print(M.github_linker())
     end
 end
 
 vim.cmd([[command! -nargs=* GithubLink lua require('nvim-github-linker').github_linker_command(<f-args>)]])
+
+return M
