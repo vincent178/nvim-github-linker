3888cc666de270f2ab5bc249457103c690af23ab
vincent178 <vh7157@gmail.com>
2023-02-19 08:17:41 +0800

    fix: range support and relative path support

--- lua/nvim-github-linker.lua
+++ lua/nvim-github-linker.lua
@@ -4,6 +4,12 @@ function M.github_linker(start_line, end_line)
     local line_number = start_line or vim.fn.line('.')
     local end_line_number = end_line or line_number
     local file_name = vim.fn.expand('%:t')
+
+    local current_file = vim.fn.expand('%:p')
+    local current_dir = vim.fn.expand('%:p:h')
+    local project_root = vim.fn.systemlist('git rev-parse --show-toplevel')[1]
+    local relative_path = vim.fn.fnamemodify(current_file, ':gs?' .. project_root .. '??')
+
     local repo_url = vim.fn.systemlist("git config --get remote.origin.url")[1]
     local repo_path = vim.fn.substitute(repo_url, '\\(.*github.com\\)\\(:\\|/\\)\\([^/]*\\)/\\(.*\\)\\.git',
         'https://github.com/\\3/\\4', '')
@@ -13,24 +19,38 @@ function M.github_linker(start_line, end_line)
     if line_number ~= end_line_number then
         range = 'L' .. line_number .. '-L' .. end_line_number
     end
-    local full_url = base_url .. '/' .. file_name .. '#' .. range
+    local full_url = base_url .. relative_path .. '#' .. range
 
     return full_url
 end
 
-function M.github_linker_command(...)
-    local args = { ... }
+function M.github_linker_command(start_line, end_line)
+    local start_line, end_line
+
+    -- Check if a range is provided
+    if vim.fn.line("'<") == 0 and vim.fn.line("'>") == 0 then
+        start_line = vim.fn.line('.')
+        end_line = start_line
+
+    else
+        -- If no range is provided, use the current line
+        start_line = vim.fn.line("'<")
+        end_line = vim.fn.line("'>")
+    end
+
     local repo = vim.fn.systemlist("git config --get remote.origin.url")[1]
     local branch = vim.fn.systemlist("git symbolic-ref --short HEAD")[1]
-    local start_line, end_line = args[1], args[2]
+
     -- save to clipboard
     if start_line and end_line then
-        vim.fn.setreg("*", M.github_linker(start_line, end_line))
+        local url = M.github_linker(start_line, end_line)
+        vim.fn.setreg("*", url)
     else
-        vim.fn.setreg("*", M.github_linker())
+        local url = M.github_linker()
+        vim.fn.setreg("*", url)
     end
 end
 
-vim.cmd([[command! -nargs=* GithubLink lua require('nvim-github-linker').github_linker_command(<f-args>)]])
+vim.cmd([[command! -range GithubLink lua require('nvim-github-linker').github_linker_command(<line1>,<line2>)]])
 
 return M
